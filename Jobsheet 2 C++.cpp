6a1c525db0db3d46d0efc3133bbacdb805d02bf8
KinkinAyuPramesty <64569553+KinkinAyuPramesty@users.noreply.github.com>
2020-05-11 16:07:58 +0700

    Create Jobsheet 2 C++.cpp
    Membuat Program Mengisi dan Menampilkan elemen Array dengan Index Array Sesuai dengan NIM Terakhir Setiap Mahasiswa

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+main()
+{
+	cout<<"NAMA : KINKIN AYU PRAMESTY \n";
+	cout<<"NIM : F1B019075 \n";
+	cout<<"KELOMPOK : 13 \n" ;
+	string x[5];
+cout<<"masukkan nim mahasiswa pertama = ";
+	cin>>x[0];
+	cout<<"masukkan nim mahasiswa kedua = ";
+	cin>>x[1];
+	cout<<"masukkan nim mahasiswa ketiga = ";
+	cin>>x[2];
+	cout<<"masukkan nim mahasiswa keempat = ";
+	cin>>x[3];
+	cout<<"masukkan nim mahasiswa kelima = ";
+	cin>>x[4];
+	cout <<"nim mahasiswa pertama = "<<x[0]<<"\n";
+	cout <<"nim mahasiswa kedua = "<<x[1]<<"\n";
+	cout <<"nim mahasiswa ketiga = "<<x[2]<<"\n";
+	cout <<"nim mahasiswa keempat = "<<x[3]<<"\n";
+	cout <<"nim mahasiswa kelima = "<<x[4]<<"\n";
+	};
